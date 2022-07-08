8b1d9f08be175d8dbe4ab8650814dee3b1a85796
Pascal Pirschel <pascal.pirschel@gmail.com>
2022-07-09 00:13:12 +0200

    Fixed missing require statement

--- src/Index.php
+++ src/Index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 use SunflowerFuchs\FakeSso\Router;
 use SunflowerFuchs\FakeSso\SsoException;
 
+require_once __DIR__ . '/SsoException.php';
 require_once __DIR__ . '/Router.php';
 require_once __DIR__ . '/User.php';
 require_once __DIR__ . '/Config.php';
